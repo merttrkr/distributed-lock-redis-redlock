0ce55c0479dbb28735ad8baaa11f547d6fa251d8
Mert Türker <45178384+merttrkr@users.noreply.github.com>
2024-10-02 01:29:41 +0300

    feat(RedisDistributedLock): distributed lock uses redlock

---
+++ Integration/Service/Abstractions/IDistributedLock.cs
@@ -0,0 +1,13 @@
+﻿using RedLockNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integration.Service.Abstractions;
+
+public interface IDistributedLock
+{
+    public Task<bool> AcquireLockAsync();
+}
